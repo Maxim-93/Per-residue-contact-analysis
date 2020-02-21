2e84b06c1c974e77012aa1cb7f63f6b0e8fdbeb7
maxim-93 <max.epstein@bioch.ox.ac.uk>
2020-02-21 17:48:22 +0000

    Updated

--- MSAs/MSA_algorithm.py
+++ MSAs/MSA_algorithm.py
@@ -13,14 +13,17 @@ from Bio.Seq import Seq
 from Bio import AlignIO
 from Bio.SeqRecord import SeqRecord
 import collections
+import numpy as np
+from Bio import SeqIO
+import os
+import subprocess
+import time
 
 align_obligate_homomer = AlignIO.read("alpha7_approved_MSA.clw", "clustal")
 align_gamma = AlignIO.read("gamma_approved_MSA.clw", "clustal")
 align_delta = AlignIO.read("delta_approved_MSA.clw", "clustal")
 align_epsilon = AlignIO.read("epsilon_approved_MSA.clw", "clustal")
 align_all = AlignIO.read("approved_MSA.clw", "clustal")
-
-# print(align_all[0])
 
 # 1) load in alignments of both heteromer and obligate homomers
 #  and combined alignments
@@ -46,34 +49,88 @@ delta=subunit_orders(subunit="ACHD")
 epsilon=subunit_orders(subunit="ACHE")
 gamma=subunit_orders(subunit="ACHG")
 
-def reorder_subs(subunit_orders):
-    subunit_block=[]
+records= list(SeqIO.parse("approved_MSA.clw",'clustal'))
+def reorder_subs(subunit_orders,subunit_name):
+    os.remove('%s'%subunit_name+'.fasta')
     for i in subunit_orders:
-        subunit_block.append(align_all[i-1])
-    return(subunit_block)
+        with open( '%s'%subunit_name+'.fasta',"a") as output_handle:
+            SeqIO.write(records[i-1],output_handle,"fasta")
+    return(list(SeqIO.parse('%s'%subunit_name+'.fasta','fasta')))
 
-alpha7_block=reorder_subs(subunit_orders=alpha7)
-delta_block=reorder_subs(subunit_orders=delta)
-epsilon_block=reorder_subs(subunit_orders=epsilon)
-gamma_block=reorder_subs(subunit_orders=gamma)
+alpha7_block=reorder_subs(subunit_orders=alpha7,subunit_name="alpha7")
+delta_block=reorder_subs(subunit_orders=delta,subunit_name="delta")
+epsilon_block=reorder_subs(subunit_orders=epsilon,subunit_name="epsilon")
+gamma_block=reorder_subs(subunit_orders=gamma,subunit_name="gamma")
+# print(alpha7_block[1].seq)
+
+# def conservation_score():
+#     for index, record in enumerate(subunit_block):
+#         print(record[0])
+# Big problem here, when you try to run $java -jar compbio-conservation-1.1.jar -i=delta.fasta -m KABAT
+#  in the command line, you get the error:
+# "Input has badly aligned sequences with columns containing nothing but the gaps. Conservation methods cannot be calculated for such an alignment !"
+# This means that you need to run this on a prealigned block, take the conservation score for that residue and map it back on to the corrosponding
+# position that matches the position for the residue with respect to all the sequences. You might be able to do this with a python dictionary.
+
+# Make a dictionary where you have a chronological list of residues with their corrosponding conservation score.
 
 # slice through each column for a particular subunit block and calculate conservation
 # for that point. Need to find a condition where if the character is an indel, give NA
 # rather than an actual conservation score.
+#
+# Get conservation score, if Counter({'-': 6}) skip that iteration.
+# if not Counter({'-': 6}), apply that conservation score to that dictionary element
 global_array=[]
 for i in range(len(alpha7_block[0])):
     temp_array=[]
     for j in range(len(alpha7_block)):
         temp_array.append(align_all[j][i])
-    # print(temp_array)
     global_array.append(temp_array)
 
+conservation_dictionary = []
 for i in global_array:
-    # print(i)
     frequencies = collections.Counter(i)
-    print(frequencies)
-# print(global_array)
+    conservation_dictionary.append(collections.Counter(i))
 
+def assign_score(subunit):
+    # os.remove('%s'%subunit+'.txt')
+    conservation=[]
+    os.system("java -jar compbio-conservation-1.1.jar -i=%s_approved_MSA.clw -m=TAYLOR_GAPS -o=%s.txt" % (subunit, subunit))
+    with open("%s.txt" % subunit, "r") as file:
+        for line in file:
+            conservation.append(line)
+    conservation=np.array(conservation)
+    conservation=np.char.split(conservation)
+    return(conservation)
+
+del_conservation=assign_score(subunit='delta')
+a7_conservation =assign_score(subunit='alpha7')
+eps_conservation=assign_score(subunit='epsilon')
+gam_conservation=assign_score(subunit='gamma')
+
+# print(len(del_conservation[0]))
+# print(len(a7_conservation[0]))
+# print(len(eps_conservation[0]))
+# print(len(gam_conservation[0]))
+
+# for i, score in enumerate(del_conservation[0]):
+    # print(score)
+    # print(i)
+print(conservation_dictionary)
+
+k=0
+for i in conservation_dictionary:
+    # print(str(i))
+    if i==conservation_dictionary[int(len(conservation_dictionary)-1)]:
+        print('oooookaaaay')
+    else:
+        # print('do something else'
+        k=k+1
+        print(del_conservation[0][k+1])
+    print(k)
+# for record in align_all:
+#     print(record.seq[:,0] + " " + record.id)
+#     print(record.seq[0:9])
 
 # You need to update  this block of code to something more sophisticated.
 # https://onlinelibrary.wiley.com/doi/full/10.1002/prot.10146#bib18
@@ -82,11 +139,6 @@ for i in global_array:
 # V_{zvelibil}=n_{const} * 1/10 (This is sort of a latex format for this equation)
 # You need to take the 'truth table' and store it as a kind of Dictionary
 
-# alpha7=align_all[0:homomer_length]
-# delta=align_all[homomer_length:homomer_length+delta_length]
-# epsilon=align_all[homomer_length+delta_length:homomer_length+delta_length+epsilon_length]
-# gamma=align_all[homomer_length+homomer_length+delta_length:homomer_length+homomer_length+delta_length+epsilon_length]
-# print(alpha7)
-# print(delta)
-# print(epsilon)
-# print(len(gamma))
+# Actually want you can do is parse a list of FastaSequence objects as an input
+# for the AACon command line method. You therefore need to find a way of saving
+# your reordered subunit blocks as a FastaSequence object first.
