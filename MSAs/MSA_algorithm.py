57e18d79eb72ef9644094746da8b65e83889724e
maxim-93 <max.epstein@bioch.ox.ac.uk>
2020-03-04 17:36:30 +0000

    bug fixing

--- MSAs/MSA_algorithm.py
+++ MSAs/MSA_algorithm.py
@@ -21,6 +21,8 @@ from Bio.SeqRecord import SeqRecord
 align_obligate_homomer = AlignIO.read("alpha7_approved_MSA.clw", "clustal")
 align_delta = AlignIO.read("delta_approved_MSA.clw", "clustal")
 align_epsilon = AlignIO.read("epsilon_approved_MSA.clw", "clustal")
+align_beta = AlignIO.read("beta_approved_MSA.clw", "clustal")
+align_gamma = AlignIO.read("gamma_approved_MSA.clw", "clustal")
 align_all = AlignIO.read("approved_MSA.clw", "clustal")
 
 # 1) load in alignments of both heteromer and obligate homomers
@@ -42,10 +44,12 @@ def subunit_orders(subunit):
 alpha7=subunit_orders(subunit="ACHA7")
 delta=subunit_orders(subunit="ACHD")
 epsilon=subunit_orders(subunit="ACHE")
+gamma=subunit_orders(subunit="ACHG")
+beta=subunit_orders(subunit="ACHB")
 
 records= list(SeqIO.parse("approved_MSA.clw",'clustal'))
 def reorder_subs(subunit_orders,subunit_name):
-    os.remove('%s'%subunit_name+'.fasta')
+    # os.remove('%s'%subunit_name+'.fasta')
     for i in subunit_orders:
         with open( '%s'%subunit_name+'.fasta',"a") as output_handle:
             SeqIO.write(records[i-1],output_handle,"fasta")
@@ -54,6 +58,10 @@ def reorder_subs(subunit_orders,subunit_name):
 alpha7_block=reorder_subs(subunit_orders=alpha7,subunit_name="alpha7")
 delta_block=reorder_subs(subunit_orders=delta,subunit_name="delta")
 epsilon_block=reorder_subs(subunit_orders=epsilon,subunit_name="epsilon")
+gamma_block=reorder_subs(subunit_orders=gamma,subunit_name="gamma")
+beta_block=reorder_subs(subunit_orders=beta,subunit_name="beta")
+
+print(beta_block)
 
 # def conservation_score():
 #     for index, record in enumerate(subunit_block):
@@ -88,13 +96,23 @@ def cons_dict(subunit, order):
 a7_cons_dict=cons_dict(subunit=alpha7_block, order=alpha7)
 d_cons_dict=cons_dict(subunit=delta_block, order=delta)
 e_cons_dict=cons_dict(subunit=epsilon_block, order=epsilon)
+g_cons_dict=cons_dict(subunit=gamma_block, order=gamma)
+b_cons_dict=cons_dict(subunit=beta_block, order=beta)
 
 # This block utilises the Barton group AACon program.
-# Make a python conservation calculator in future.
+# Try to put your own kabat function in rather than relying on AACon in the future.
+
+# VKabat= k/n1 * N
+# where k is the number of amino types present at the
+# aligned position, n1 is the number of times the most
+# commonly occurring amino acid appears there, and N is
+# the number of sequences in the alignment. The variable N
+# acts as a scaling factor and is constant for a given
+# alignment. (PROTEINS: Structure, Function, and Genetics 48:227–241 (2002))
+
 def assign_score(subunit):
     os.remove('%s'%subunit+'.txt')
     conservation=[]
-    # os.system("java -jar compbio-conservation-1.1.jar -i=%s_approved_MSA.clw -m=TAYLOR_GAPS -o=%s.txt" % (subunit, subunit))
     os.system("java -jar compbio-conservation-1.1.jar -i=%s_approved_MSA.clw -m=KABAT -o=%s.txt" % (subunit, subunit))
 
     with open("%s.txt" % subunit, "r") as file:
@@ -107,6 +125,8 @@ def assign_score(subunit):
 del_conservation=assign_score(subunit='delta')
 a7_conservation =assign_score(subunit='alpha7')
 eps_conservation=assign_score(subunit='epsilon')
+gam_conservation=assign_score(subunit='gamma')
+beta_conservation=assign_score(subunit='beta')
 
 # determine what residues of the global alignment to skip over for that particular subunit block
 # when attributing a conservation score to the non-global alignment.
@@ -120,12 +140,13 @@ def skip_number(subunit_length, subunit):
                 gap_positions.append(h)
         h=h+1
     gap_positions =list(dict.fromkeys(gap_positions))
-
     return(gap_positions)
 
 a7_skip=skip_number(subunit_length=len(alpha7_block), subunit=a7_cons_dict)
 eps_skip=skip_number(subunit_length=len(epsilon_block), subunit=e_cons_dict)
 del_skip=skip_number(subunit_length=len(delta_block), subunit=d_cons_dict)
+gam_skip=skip_number(subunit_length=len(gamma_block), subunit=g_cons_dict)
+# beta_skip=skip_number(subunit_length=len(beta_block), subunit=b_cons_dict)
 
 # As ?_conservation dictionary (contains the scores) only considers local alignment,
 # you need to go through the ?_conservation dictionaries and append that score to the
@@ -145,7 +166,11 @@ def local_append(dictionary, local_score, skip):
 
 a7_local_dict= local_append(dictionary=a7_cons_dict,local_score=a7_conservation,  skip=a7_skip )
 del_local_dict=local_append(dictionary=d_cons_dict, local_score=del_conservation, skip=del_skip)
+# beta_local_dict=local_append(dictionary=b_cons_dict, local_score=beta_conservation, skip=beta_skip)
 eps_local_dict=local_append(dictionary=e_cons_dict, local_score=eps_conservation, skip=eps_skip)
+# gam_local_dict=local_append(dictionary=g_cons_dict, local_score=gam_conservation, skip=gam_skip)
+
+# print(del_local_dict)
 
 # Gamma doesn't seem to work for some reason for local_append function.
 # Now that you have the local scores for each subunit, you need to
@@ -158,6 +183,7 @@ eps_local_dict=local_append(dictionary=e_cons_dict, local_score=eps_conservation
 # [1,2,7,4,-,1,2,9,5]
 # [2,-,2,1,4,7,8,5,-]
 
+# I think this function is slightly flawed. Have a rethink....
 def stoich_score(homomer, heteromer):
     score=[]
     for i in range(len(homomer)):
@@ -166,17 +192,16 @@ def stoich_score(homomer, heteromer):
         elif homomer[i]['local score'] == 0 or heteromer[i]['local score'] == 0:
             score.append('non conserved gap')
         else:
-            # print(homomer[i]['local score'])
-            # print(heteromer[i]['local score'])
-            # print(int(homomer[i]['local score'])*int(heteromer[i]['local score'])
             score.append(float(homomer[i]['local score'])*float(heteromer[i]['local score']))
     return(score)
 
 del_stoich_score=stoich_score(homomer=a7_local_dict, heteromer=del_local_dict)
 eps_stoich_score=stoich_score(homomer=a7_local_dict, heteromer=eps_local_dict)
 
-print(eps_stoich_score)
-# print(a7_local_dict)
+# beta_stoich_score=stoich_score(homomer=a7_local_dict, heteromer=beta_local_dict)
+# print(del_stoich_score)
+# print(eps_stoich_score)
+# print(beta_stoich_score)
 
 # You need to update  this block of code to something more sophisticated.
 # https://onlinelibrary.wiley.com/doi/full/10.1002/prot.10146#bib18
